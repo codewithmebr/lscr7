dfcc5417412a10dcfcbaaa748804f22fbff5d0bd
CodeWithMeBR <codewithmebr@gmail.com>
2025-12-29 17:33:21 -0300

    lscr7

---
+++ lscr7.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash 
+
+COMMAND=$1
+
+#Print cr7
+chafa /home/rodrigo/codewithmebr/01/cr7.png
+
+#Exibindo o comando
+echo "[codewithMeBR]: $COMMAND"
+echo
+
+#Executando o comando 
+$COMMAND
